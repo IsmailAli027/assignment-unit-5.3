1621ede57d9e2a5073b3f8c215a849e7f5b3b148
Ismail Ali <Ismailali027@gmail.com>
2023-02-19 17:14:05 -0600

    wrote code to add more albums to my object array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -19,3 +19,16 @@ function addToCollection(title, artist, yearPublished){
 }
 console.log('My music collection:', addToCollection( 'Take Care', 'Drake', 2011) );
 console.log('The first album is:', collection[0]);
+
+
+// - Test the `addToCollection` function:
+//   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+//   - Console.log each album as added using the returned value.
+//   - After all are added, console.log the `collection` array
+console.log(addToCollection('The College Dropout', 'Kanye', 2004));
+console.log(addToCollection('The Carter 3', 'Lil Wayne', 2008));
+console.log(addToCollection('Its Only Me', 'Lil Baby', 2022));
+console.log(addToCollection('Riot', 'Paramore', 2013));
+console.log(addToCollection('Dangerous', 'Morgan Wallen', 2021));
+console.log(addToCollection('All Eyez on Me', 'Tupac', 1996));
+console.log('My music collection:', collection);
